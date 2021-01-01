54f48fc66390cf2b042c91990dcfedc721233284
XJTU-maomao <1406851996@qq.com>
2021-01-01 19:43:31 +0800

    initial

---
+++ com/maomao/leetcode/T605/Solution1.java
@@ -0,0 +1,11 @@
+package maomao.leetcode.T605;
+
+public class Solution1 {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
