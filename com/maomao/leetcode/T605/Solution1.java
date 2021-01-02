d906fa7b67cb1f2c67cd275d0f86da2946319bdf
XJTU-maomao <1406851996@qq.com>
2021-01-02 09:38:11 +0800

    test

--- com/maomao/leetcode/T605/Solution1.java
+++ com/maomao/leetcode/T605/Solution1.java
@@ -16,7 +16,6 @@ public class Solution1 {
             flowerbed[flowerbed.length-1] = 1;
             n--;
         }
-        //test
         for (int i = 1; i < flowerbed.length-1; i++) {
             if (flowerbed[i] == 1){
                 i += 1;
